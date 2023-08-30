191bcde6d4ff80c84bd12723a0cfa4980f781b8b
manasisahu <manasi.sahu2009@gmail.com>
2023-08-30 22:10:04 +1200

    Add files via upload

---
+++ TravelInsurance/GetData.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelInsurance
+{
+    public class GetData
+    {
+        public TravelResponseDTO getInsuranceData()
+        {
+            var client = new RestClient("https://api.tmsandbox.co.nz");
+
+            var categoriesrequest = new RestRequest("/v1/Categories/6327/Details.json", Method.GET);
+            categoriesrequest.AddHeader("Accept", "application/json");
+            categoriesrequest.AddQueryParameter("catalogue", "false");
+            categoriesrequest.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = client.Execute(categoriesrequest);
+            var responseContent = response.Content;
+            var travelData = JsonConvert.DeserializeObject<TravelResponseDTO>(responseContent);
+            return travelData;
+
+        }
+    }
+}
