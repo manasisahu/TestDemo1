2a7be707723c1f98db1f7293b0f20a4477a7b882
manasisahu <manasi.sahu2009@gmail.com>
2023-08-30 22:10:58 +1200

    Add files via upload

---
+++ TravelInsuranceTest/APIUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using TravelInsurance;
+
+namespace TravelInsuranceTest
+{
+    [TestClass]
+    public class APIUnitTest
+    {
+        [TestMethod]
+        public void WelcomeNameTest()
+        {
+            var getData = new GetData();
+            var response = getData.getInsuranceData();
+            Assert.AreEqual("Carbon credits", response.Name);
+        }
+
+        [TestMethod]
+        public void WelcomeCanRelistTest()
+        {
+            var getData = new GetData();
+            var response = getData.getInsuranceData();
+            Assert.IsTrue(response.CanRelist);
+        }
+        [TestMethod]
+        public void PromotionGalleryDescriptionTest()
+        {
+            var getData = new GetData();
+            var response = getData.getInsuranceData();
+            response.Promotions.Where(promotion =>promotion.Name == "Gallery").ToList().ForEach(delegate(Promotion promo) {
+                Assert.AreEqual("Good position in category", promo.Description);
+            });
+        }
+    }
+}
---
+++ TravelInsuranceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TravelInsuranceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("TravelInsuranceTest")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("09bad58a-30c6-42a8-b8f5-5b47fac26baa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
